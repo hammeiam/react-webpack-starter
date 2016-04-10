0176ee7ab9a2c0e1b18cfeace2c7312b9dc57d8e
David Hamme <dhamme@gmail.com>
2016-04-09 21:07:52 -0700

    tabs to spaces

--- src/ExampleComponent.jsx
+++ src/ExampleComponent.jsx
@@ -2,23 +2,23 @@ import React, { PropTypes, Component } from 'react'
 import image from './rambo.jpg'
 
 class MyComponent extends Component {
-	render() {
-		const { message } = this.props
-		return (
-			<div>
-				<div>{message}</div>
-				<img src={image} />
-			</div>
-		)
-	}
+  render() {
+    const { message } = this.props
+    return (
+      <div>
+        <div>{message}</div>
+        <img src={image} />
+      </div>
+    )
+  }
 }
 
 MyComponent.defaultProps = {
-	message: 'You\'re good to go!'
+  message: 'You\'re good to go!'
 }
 
 MyComponent.proptypes = {
-	message: PropTypes.string.isRequired
+  message: PropTypes.string.isRequired
 }
 
 export default MyComponent
